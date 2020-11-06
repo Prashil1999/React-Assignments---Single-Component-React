06dc80de1cfe59bf40dc83f398037d454f616234
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-11-06 14:21:59 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  let text = "I am learning React. My life is getting better. ";
   return (
-    <></>
-  )
-}
-
+    <div id="root">
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default App;
